b748653bcd72af9057ccfad8ad5eb394df2cdfaf
TakedaShingem <99263117+TakedaShingem@users.noreply.github.com>
2022-02-08 10:34:08 -0300

    Adicionei arquivos via GitHub

---
+++ Hello GitHub/src/br/com/matheus/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.matheus.git;
+
+/**
+ * @author Matheus Rodrigues
+ * @version 1.1
+ * @date 2022.02.08
+ * Wmxemplo de integração do Eclipse com o GitHub
+ *
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
